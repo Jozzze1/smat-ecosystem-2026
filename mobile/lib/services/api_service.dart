845d3474d71cfd63318a8d3d2488e0c1479e3c80
Jozzze1 <jose.pacarap@unmsm.edu.pe>
2026-05-06 10:38:22 -0500

    Commit Labo 6.2 completado

--- mobile/lib/services/api_service.dart
+++ mobile/lib/services/api_service.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/estacion.dart';
+import 'auth_service.dart'; // 👈 IMPORTANTE
 
 class ApiService {
   final String baseUrl = "http://192.168.121.46:8000";
 
+  // 🔹 GET (ya lo tenías)
   Future<List<Estacion>> fetchEstaciones() async {
     final response = await http.get(
       Uri.parse('$baseUrl/estaciones/'),
@@ -19,5 +21,24 @@ class ApiService {
     } else {
       throw Exception('Error al conectar con el servidor SMAT');
     }
+  }
+
+  // NUEVO: POST protegido
+  Future<bool> crearEstacion(String nombre, String ubicacion) async {
+    final token = await AuthService().getToken();
+
+    final response = await http.post(
+      Uri.parse('$baseUrl/estaciones/'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode({
+        'nombre': nombre,
+        'ubicacion': ubicacion,
+      }),
+    );
+
+    return response.statusCode == 200;
   }
 }
